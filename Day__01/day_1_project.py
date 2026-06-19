04c9601e6c95d839fc98b069fb3e72d591feced3
AjitKumarDev <198491058+AjitKumarDev@users.noreply.github.com>
2026-06-19 19:23:58 +0530

    Update day_1_project.py

--- Day__01/day_1_project.py
+++ Day__01/day_1_project.py
@@ -1,1 +1,17 @@
 print("Hello World! Day 1 of 100 has started. Let's go!")
+
+# Output Prints:
+name = "Ajit"
+print(name)
+
+name="Ravi"
+print(name)
+
+# Taking inputs:---- string as default value
+
+your_name = input("What is your name? ")
+print(your_name)
+
+# Get length of the string
+length = len(your_name)
+print(length)
